00f8e8150aab29a349dbba244cfa980b46a54841
Shunyao Liang <shunyao.liang@education.nsw.gov.au>
2019-11-02 06:58:04 +1100

    Files

---
+++ player.c
@@ -0,0 +1,31 @@
+#include "torch.h"
+#include "list.h"
+
+struct entity player = {
+	.r = 151, .g = 151, .b = 151,
+	.token = '@',
+	.posy = 17, .posx = 28,
+	.update = demo_player_update,
+	.destroy = NULL,
+	.list = LIST_HEAD_INIT(player.list),
+};
+
+def_main_win_key_fn(player_move_left)
+{
+	entity_move_pos_rel(&player, 0, -1);
+}
+
+def_main_win_key_fn(player_move_down)
+{
+	entity_move_pos_rel(&player, 1, 0);
+}
+
+def_main_win_key_fn(player_move_up)
+{
+	entity_move_pos_rel(&player, -1, 0);
+}
+
+def_main_win_key_fn(player_move_right)
+{
+	entity_move_pos_rel(&player, 0, 1);
+}
