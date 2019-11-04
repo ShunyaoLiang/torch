673ce07f17fb9616d4756745fa80dcee9b3025c9
Shunyao Liang <shunyao.liang@education.nsw.gov.au>
2019-11-04 11:07:21 +1100

    Removed reserved '__' prefix with 'intern_'

--- floor.c
+++ floor.c
@@ -10,26 +10,26 @@ typedef cell cell_grid[MAP_LINES][MAP_COLS];
 #define cell_grid_for_each_cell(pos, grid) \
 	for (pos = *grid; pos != back(grid); ++pos)
 
-static void __populate_grid(cell_grid grid, float rate);
-static void __iterate_grid(cell_grid grid, int birth, int survive);
-static int __cell_alive_neighbours(cell_grid grid, int y, int x);
-static int __cell_grid_at(cell_grid grid, int y, int x);
+static void intern_populate_grid(cell_grid grid, float rate);
+static void intern_iterate_grid(cell_grid grid, int birth, int survive);
+static int intern_cell_alive_neighbours(cell_grid grid, int y, int x);
+static int intern_cell_grid_at(cell_grid grid, int y, int x);
 
-static void __floor_write_grid(struct floor *floor, cell_grid grid);
+static void intern_floor_write_grid(struct floor *floor, cell_grid grid);
 
 void floor_map_generate(struct floor *floor, enum floor_type type)
 {
 	cell_grid grid = { 0 };
-	__populate_grid(grid, 0.45f);
+	intern_populate_grid(grid, 0.45f);
 
 	for (int i = 0; i < 12; ++i) {
-		__iterate_grid(grid, 4, 5);
+		intern_iterate_grid(grid, 4, 5);
 	}
 
-	__floor_write_grid(floor, grid);	
+	intern_floor_write_grid(floor, grid);	
 }
 
-static void __populate_grid(cell_grid grid, float rate)
+static void intern_populate_grid(cell_grid grid, float rate)
 {
 /*
 	cell *pos;
@@ -50,12 +50,12 @@ static void __populate_grid(cell_grid grid, float rate)
 	}
 }
 
-static void __iterate_grid(cell_grid grid, int birth, int survive)
+static void intern_iterate_grid(cell_grid grid, int birth, int survive)
 {
 	cell_grid new = { 0 };
 	for (int y = 0; y < MAP_LINES; ++y)
 		for (int x = 0; x < MAP_COLS; ++x) {
-			int alive = __cell_alive_neighbours(grid, y, x);
+			int alive = intern_cell_alive_neighbours(grid, y, x);
 			if (grid[y][x]) {
 				if (alive >= survive)
 					new[y][x] = 1;
@@ -72,7 +72,7 @@ static void __iterate_grid(cell_grid grid, int birth, int survive)
 		}
 }
 
-static int __cell_alive_neighbours(cell_grid grid, int y, int x)
+static int intern_cell_alive_neighbours(cell_grid grid, int y, int x)
 {
 	int alive = 0;
 /*
@@ -83,19 +83,19 @@ static int __cell_alive_neighbours(cell_grid grid, int y, int x)
 		}
 */
 
-	__cell_grid_at(grid, y - 1, x - 1) ? alive++ : 0;
-	__cell_grid_at(grid, y - 1, x) ? alive++ : 0;
-	__cell_grid_at(grid, y - 1, x + 1) ? alive++ : 0;
-	__cell_grid_at(grid, y, x - 1) ? alive++ : 0;
-	__cell_grid_at(grid, y, x + 1) ? alive++ : 0;
-	__cell_grid_at(grid, y + 1, x - 1) ? alive++ : 0;
-	__cell_grid_at(grid, y + 1, x) ? alive++ : 0;
-	__cell_grid_at(grid, y + 1, x + 1) ? alive++ : 0;
+	intern_cell_grid_at(grid, y - 1, x - 1) ? alive++ : 0;
+	intern_cell_grid_at(grid, y - 1, x) ? alive++ : 0;
+	intern_cell_grid_at(grid, y - 1, x + 1) ? alive++ : 0;
+	intern_cell_grid_at(grid, y, x - 1) ? alive++ : 0;
+	intern_cell_grid_at(grid, y, x + 1) ? alive++ : 0;
+	intern_cell_grid_at(grid, y + 1, x - 1) ? alive++ : 0;
+	intern_cell_grid_at(grid, y + 1, x) ? alive++ : 0;
+	intern_cell_grid_at(grid, y + 1, x + 1) ? alive++ : 0;
 	
 	return alive;
 }
 
-static int __cell_grid_at(cell_grid grid, int y, int x)
+static int intern_cell_grid_at(cell_grid grid, int y, int x)
 {
 	if (floor_map_in_bounds(y, x))
 		return grid[y][x];
@@ -103,7 +103,7 @@ static int __cell_grid_at(cell_grid grid, int y, int x)
 		return 1;
 }
 
-static void __floor_write_grid(struct floor *floor, cell_grid grid)
+static void intern_floor_write_grid(struct floor *floor, cell_grid grid)
 {
 	struct tile *t;
 	cell *c = *grid;
